7c59453a30024f7ab825eb10e5d595c9dc6a80f9
Georgi_BulPros <georgi.bozhilov@bulpros.com>
2019-07-30 18:25:12 +0300

    Add logic CRUD logic for movies.

--- DNC.Example.API/Controllers/MoviesController.cs
+++ DNC.Example.API/Controllers/MoviesController.cs
@@ -1,46 +1,104 @@
-﻿
-namespace DNC.Example.API.Controllers
+﻿namespace DNC.Example.API.Controllers
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
+
+    using Microsoft.AspNetCore.Mvc
+        ;
+    using DNC.Example.DAL.Context;
+    using DNC.Example.DAL.Models;
+    using DNC.Example.DAL.DTOs;
 
     [Route("api/[controller]")]
     [ApiController]
     public class MoviesController : ControllerBase
     {
+        private readonly MovieListDbContext _context;
+
+        public MoviesController(MovieListDbContext context)
+        {
+            this._context = context;
+        }
+
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Movie>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var listOfmovies = _context.Movies.ToList();
+
+            return listOfmovies;
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Movie> Get(int id)
         {
-            return "value";
+            var foundMovie = _context.Movies.ToList().FirstOrDefault(x => x.Id == id);
+
+            return foundMovie;
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public void Post([FromBody] MovieDTO value)
+        {            
+            if (ModelState.IsValid)
+            {
+                //remove: data:image/png;base64,
+                var onlyContent = value.ImageAsBase64.Substring(22);
+                byte[] image = Convert.FromBase64String(onlyContent);
+
+                Movie newMovie = new Movie
+                {
+                    Title = value.Title,
+                    Year = value.Year,
+                    ScoreRating = value.ScoreRating,
+                    Image = image
+                };
+
+                _context.Add(newMovie);
+                _context.SaveChanges();
+            }
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] MovieDTO value)
         {
+            if (ModelState.IsValid)
+            {
+                //remove: data:image/png;base64,
+                var onlyContent = value.ImageAsBase64.Substring(22);
+                byte[] image = Convert.FromBase64String(onlyContent);
+
+                var foundMovie = _context.Movies.ToList().FirstOrDefault(x => x.Id == id);
+
+                if (foundMovie != null)
+                {
+                    foundMovie.Title = value.Title;
+                    foundMovie.Year = value.Year;
+                    foundMovie.ScoreRating = value.ScoreRating;
+                    foundMovie.Image = image;
+
+                    _context.Movies.Update(foundMovie);
+                    _context.SaveChanges();
+                }
+            }
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var foundMovie = _context.Movies.ToList().FirstOrDefault(x => x.Id == id);
+            if (foundMovie != null)
+            {
+                _context.Movies.Remove(foundMovie);
+                _context.SaveChanges();
+            }
+
         }
     }
 }
---
+++ DNC.Example.DAL/DTOs/MovieDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DNC.Example.DAL.DTOs
+{
+    public class MovieDTO
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public float ScoreRating { get; set; }
+        public string ImageAsBase64 { get; set; }
+    }
+}
